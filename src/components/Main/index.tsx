7cf6a3a2fe7b8ce743595d38350a99b9a85b4906
Melkcampos <melquisedequecampos.trabalho@gmail.com>
2022-07-06 00:10:14 -0300

    twitter

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import ProfilePage from '../ProfilePage';
+
+import { 
+        Container,
+        Header,
+        BackIcon,
+        ProfileInfo,
+        SearchIcon,
+        BottomMenu,
+        HomeIcon,
+        BellIcon,
+        EmailIcon
+} from './styles';
+
+// Parte de cima do perfil do usuario
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+        <Header>
+            <button>
+                <BackIcon />
+            </button>
+
+            <ProfileInfo>
+                <strong> Nome do Usuario </strong>
+                <span> 612 Tweets </span>
+            </ProfileInfo>
+        </Header>
+
+        <BottomMenu>
+            <HomeIcon />
+            <SearchIcon />
+            <BellIcon />
+            <EmailIcon />
+        </BottomMenu>
+
+    </Container>
+  );
+}
+
+export default Main;
---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const ProfilePage: React.FC = () => {
+  return <div />;
+}
+
+export default ProfilePage;
+
+// parei em 43:17
