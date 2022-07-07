4f8fe1b60d794499bef1f4af5b9ebb0c7f197c4a
Melkcampos <melquisedequecampos.trabalho@gmail.com>
2022-07-07 18:12:37 -0300

    twitter

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import {
+   Container,
+   Retweeted,
+   ProjectIcon,
+   Body,
+   Avatar,
+   Content,
+   Header,
+   Dot,
+   Description,
+   ImageContent,
+   Icons,
+   Status,
+   CommentIcon,
+   RetweetIcon,
+   LikeIcon, 
+} from './styles';
 
 const Tweet: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Retweeted>
+        <ProjectIcon />
+        Você retweetou
+      </Retweeted>
+
+      <Body>
+        <Avatar />
+
+        <Content>
+          <Header>
+            <strong> Twitter clone project </strong>
+            <span> @nasa_404 </span>
+            <Dot />
+            <time>07 de jul</time>
+          </Header>
+
+          <Description> Descrição do tweet 🦷 </Description>
+          <ImageContent />
+
+          <Icons>
+            <Status>
+              <CommentIcon />
+              32
+            </Status>
+
+            <Status>
+              <RetweetIcon />
+              11
+            </Status>
+
+            <Status>
+              <LikeIcon />
+              562
+            </Status>
+
+          </Icons>
+  
+        </Content>
+      </Body>  
+      </Container>
+  );
 }
 
 export default Tweet;
